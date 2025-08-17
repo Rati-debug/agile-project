76138a9ffb3b091195d780491ad0d0dd58644688
Rati Ranjan Biswal <ranjanrati@2002.com>
2025-08-17 12:55:03 +0530

    commited 1

--- CollegeERP/settings.py
+++ CollegeERP/settings.py
@@ -138,3 +138,15 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
 }
+# CollegeERP/settings.py
+import os
+DATABASES = {
+  "default": {
+    "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
+    "NAME": os.getenv("DB_NAME", os.path.join(BASE_DIR, "db.sqlite3")),
+    "USER": os.getenv("DB_USER", ""),
+    "PASSWORD": os.getenv("DB_PASSWORD", ""),
+    "HOST": os.getenv("DB_HOST", ""),
+    "PORT": os.getenv("DB_PORT", ""),
+  }
+}
